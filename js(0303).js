bcef7f048008866b69db17602c945d4c07f60be9
Lawrense999 <99010181+Lawrense999@users.noreply.github.com>
2022-03-03 23:54:28 +0900

    Learn JS.
    "var" is not used recently, instead, "const", "let" is used mostly.
    If you wanna see the result, put "console.log()"
    alert : alarm to users.
    prompt : get a text from users.
    confirm : get a confirm from users.
    It is a String that get from prompt.
    You have to write in a uppercase if you wanna put a "String", "Number", etc..
    Number(true) = 1 
    Number(false) = 0 
    
    Boolean(0)
    Boolean("")
    Boolean(null)
    Boolean(undefined)
    Boolean(NaN) 
    All false.
    
    String() = 문자형으로 변환
    Number() = 숫자형으로 변환 (문자가 들어갈 경우 NaN)
    Boolean() = 불린형으로 변환
    
    Number(null) // 0
    Number(undefined) // NaN
    ** = 제곱근

---
+++ js(0303).js
@@ -0,0 +1,35 @@
+console.log('Hello, World!');
+// 99단 : for(var i=2; i < 10 ; i++) {
+//     for (var j=1; j < 10; j++) {
+//       console.log(i+"*"+j+"="+i*j);
+//     }
+//   }
+//   var은 잘안쓰이고 let, const가 이젠 쓰임
+//   출력값을 보고 싶을 땐 console.log()
+
+//   alert : 알려줌
+// prompt : 입력 받음
+// confirm : 확인 받음
+
+// prompt로 입력받은 값은 무조건 "문자"형
+
+// String, Number 모두 첫글자는 대문자로 써야함
+
+// Number(true) = 1 
+// Number(false) = 0 의 값을 가짐
+
+// Boolean(0)
+// Boolean("")
+// Boolean(null)
+// Boolean(undefined)
+// Boolean(NaN)
+// 모두 false가 됨.
+
+// String() = 문자형으로 변환
+// Number() = 숫자형으로 변환 (문자가 들어갈 경우 NaN)
+// Boolean() = 불린형으로 변환
+
+// Number(null) // 0
+// Number(undefined) // NaN
+
+// ** = 제곱근
